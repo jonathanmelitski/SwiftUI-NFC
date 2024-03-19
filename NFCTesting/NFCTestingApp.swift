f0885e0a504df0ed9d886f2d30cc207d6a588903
jmelitski <jonathanmelitski@gmail.com>
2024-03-18 22:21:46 -0400

    Initial Commit

---
+++ NFCTesting/NFCTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCTestingApp.swift
+//  NFCTesting
+//
+//  Created by Jon Melitski on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NFCTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
